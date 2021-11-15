1144555da894d4ccbd2144d7702d34afb3e777bf
marwenda <marwenda55@gmail.com>
2021-11-15 13:16:59 +0100

    interfaz grafica simple

--- Excell.py
+++ Excell.py
@@ -5,8 +5,10 @@
 
 #Creamos una execpcion por si falla la apertura de la libreria.
 try:
+    import sys
     from openpyxl.utils.cell import get_column_letter
     from openpyxl import Workbook, load_workbook
+    from tkinter import *
 except ImportError:
     # Que hacer si el módulo no se puede importar
     print("Módulo no instalado")
@@ -61,9 +63,49 @@ def Printarventa(NumeroFila):
 #Usamos el rango de 2,122 para sacar las primeras 121 Facturas simplificadas.
 #Funciona perfectamente.
 
-for i in range(226,227): # ACUERDATE de poner un numero mas de la fila ultima del registro.
-    Printarventa(str(i))
+#for i in range(226,227): # ACUERDATE de poner un numero mas de la fila ultima del registro.
+ #   Printarventa(str(i))
 
 
-#TODO Necesitamos interfaz grafica, y un panel de opciones para configurar tanto la celda que queramos para obtener
-# los datos como para que celda rellenar de forma sencilla.    
+def sumar():
+    try:
+        _valor1 = int(entrada_texto.get())
+        _valor2 = int(entrada2_texto.get())
+        for i in range(_valor1,_valor2): # ACUERDATE de poner un numero mas de la fila ultima del registro.
+           Printarventa(str(i))
+        etiqueta4.config(text="Exito")
+    except ValueError:
+        etiqueta4.config(text="Introduce un numero")
+
+
+
+
+app = Tk()
+app.title("Facturas Simplif")
+
+# Ventana Principal
+vp = Frame(app)
+vp.grid(column=0, row=0, padx=(50, 50), pady=(10, 10))
+vp.columnconfigure(0, weight=1)
+vp.rowconfigure(0, weight=1)
+
+etiqueta = Label(vp, text="Desde la fila ")
+etiqueta.grid(column=1, row=1, sticky=(W, E))
+etiqueta2 = Label(vp, text="Hasta la fila")
+etiqueta2.grid(column=3, row=1, sticky=(W, E))
+
+etiqueta4= Label(vp, text="ACUERDATE de poner un numero mas de la fila ultima del registro.")
+etiqueta4.grid(column=2, row=5, sticky=(W, E))
+
+boton = Button(vp, text="Crear facturas", command=sumar)
+boton.grid(column=1, row=4)
+
+valor = "desde"
+entrada_texto = Entry(vp, width=10, textvariable=valor)
+entrada_texto.grid(column=2, row=1)
+valor2 = "hasta"
+entrada2_texto = Entry(vp, width=10, textvariable=valor2)
+entrada2_texto.grid(column=4, row=1)
+
+
+app.mainloop()
